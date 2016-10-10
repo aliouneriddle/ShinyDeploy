005229d1db8430c6b8d8f2e870f087a89d0cde9d
aliouneriddle <aliouneriddle@yahoo.fr>
2016-10-10 01:12:39 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,29 +1,55 @@
 library(shiny)
-library(ggplot2)
 
-dataset <- diamonds
 
-shinyUI(pageWithSidebar(
-  
-  headerPanel("Diamonds Explorer"),
-  
+shinyUI(fluidPage(
+  titlePanel("Calculs Boursiers !"),br(),
   sidebarPanel(
+    selectInput(inputId = "nom_valeur", label = "Valeur",
+                choices = c("CABC","FTSC","NEIC","NTLC","SEMC","SIVC","SLBC","SMBC","STBC","TTRC","UNLC","CABC","CIEC","ONTBF","SDCC","SNTS","BICC","BOAB","BOABF","BOAC","BOAN","BOAS","ETIT","SAFC","SGBC","SDSC","SVOC","PALC","SICC","SOGC","SPHC","ABJC","BNBC","CFAC","PRSC","SHEC","TTLC","TTLS","STAC"),
+                selected = "CABC"),
+    numericInput("coursha","Cours achat",52,0,500000,1,width=NULL),
+    numericInput("nbre","Nbre d'action",45500,40,100000,1,width=NULL),
+    numericInput("coursvte","Cours vente",54,0,500000,1,width=NULL),
+    checkboxInput(inputId = "individual_obs",
+                  label = strong("Show individual observations"),
+                  value = FALSE),
+    actionButton("update", "Update Table"),
+    downloadButton('downloadData', 'Download')
     
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(1000, nrow(dataset)), step=500, round=0),
-    
-    selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-    
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
   ),
   
   mainPanel(
-    plotOutput('plot')
-  )
-))
+    
+    tabsetPanel(id = "tabs",
+                tabPanel("Paramètres", value = "A", h3("Table de paramétrage"),hr(),br(),
+                         fluidRow(
+                           column(width=2,numericInput("neg", "Négociation", 0.014, 0, 1, step = 0.001,width = NULL)),
+                           column(width=2,numericInput("val", "Valorisation", 0.025, 0,1, step = 0.001,width = NULL)),
+                           column(width=2,numericInput("taf", "TAF", 0.17, 0, 1, 0.01,width = NULL)),
+                           column(width=2,numericInput("btaf", "Base TAF", 0.011, 0, 1, 0.001,width = NULL))
+                         ),
+                         fluidRow(column(width=2,numericInput("btdaf", "Deuxième ligne", 0.011, 0, 1, 0.001,width = NULL))),
+                         hr(),
+                         h4("Troisième ligne"),
+                         fluidRow(column(width=3,br(),verbatimTextOutput("resultat"))),
+                         hr(),fluidRow(column(width=2,tableOutput("cout_achat")))
+                ),
+                #fluidRow(
+                #column(width=2,nnumericInput("taf", "TAF", 0.17, 0, 1, 0.01,width = NULL))
+                #column(width=2,numericInput("btaf", "Base TAF", 0.011, 0, 1, 0.001,width = NULL
+                #fluidRow(
+                #column(width=2,br(),verbatimTextOutput("rlst"))#,
+                #dataTableOutput("tab")
+                tabPanel("Simulation", value = "D",hr(),h3("Résultat de la simulation")
+                         #fluidRow(column(width=2,tableOutput("cout_achat")))
+                ),
+                tabPanel("Prévisions", value = "C", hr(),h3("Résultat de la prévision")),
+                #fluidRow(
+                #column(width=2,numericInput("negF", "Négociation", 0.014, 0, 1, step = 0.001,width = NULL)),
+                #column(width=2,verbatimTextOutput("negoc"))
+                #)
+                tabPanel("Panorama", value = "D",hr(),h3("Portefeuille de valeurs"),
+                         tableOutput("table1")),
+                #fluidRow(column(width=2,verbatimTextOutput("vachats")))),
+                tabPanel("Graphiques", value = "E", br(),br(),plotOutput("pl"))
+    )))
